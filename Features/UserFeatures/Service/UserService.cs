96e4318510e6656d85d3539194c16d35ab1cfcfd
Ấn Trần <tquocan04@gmail.com>
2025-05-05 22:45:16 +0700

    fix register

--- Features/UserFeatures/Service/UserService.cs
+++ Features/UserFeatures/Service/UserService.cs
@@ -20,89 +20,89 @@ namespace Features.UserFeatures.Service
             await _repositoryManager.User.Create(newUser);
 
             // dailyplan
-            var today = DateOnly.FromDateTime(DateTime.Today);
+            //var today = DateOnly.FromDateTime(DateTime.Today);
 
-            if (newUser.Time == 0 || newUser.TDEE == null || newUser.Weight == null)
-            {
-                return null;
-            }
+            //if (newUser.Time == 0 || newUser.TDEE == null || newUser.Weight == null)
+            //{
+            //    return null;
+            //}
 
-            float caloPerDay = 7700 / newUser.Time;
+            //float caloPerDay = 7700 / newUser.Time;
 
-            float calo = (float)newUser.TDEE; // calo
-            float fat = (float)newUser.Weight; // gram
-            float protein = (float)newUser.Weight; // gram
+            //float calo = (float)newUser.TDEE; // calo
+            //float fat = (float)newUser.Weight; // gram
+            //float protein = (float)newUser.Weight; // gram
 
-            if (newUser.Weight < newUser.TargetWeight) // tang can
-            {
-                calo += caloPerDay;
-                fat *= 0.33f;
-            }
-            else if (newUser.Weight > newUser.TargetWeight) // giam can
-            {
-                calo -= caloPerDay;
-                fat *= 0.23f;
-                protein *= 1.1f;
-            }
-            else // giu can
-            {
-                fat *= 0.33f;
-            }
+            //if (newUser.Weight < newUser.TargetWeight) // tang can
+            //{
+            //    calo += caloPerDay;
+            //    fat *= 0.33f;
+            //}
+            //else if (newUser.Weight > newUser.TargetWeight) // giam can
+            //{
+            //    calo -= caloPerDay;
+            //    fat *= 0.23f;
+            //    protein *= 1.1f;
+            //}
+            //else // giu can
+            //{
+            //    fat *= 0.33f;
+            //}
 
-            Guid dailyId = Guid.NewGuid();
+            //Guid dailyId = Guid.NewGuid();
 
-            var breakfast = new Breakfast
-            {
-                TotalCalories = 0,
-                TotalCarbs = 0,
-                TotalFats = 0,
-                TotalProteins = 0,
-                DailyPlan_id = dailyId,
-            };
+            //var breakfast = new Breakfast
+            //{
+            //    TotalCalories = 0,
+            //    TotalCarbs = 0,
+            //    TotalFats = 0,
+            //    TotalProteins = 0,
+            //    DailyPlan_id = dailyId,
+            //};
 
-            var lunch = new Lunch
-            {
-                TotalCalories = 0,
-                TotalCarbs = 0,
-                TotalFats = 0,
-                TotalProteins = 0,
-                DailyPlan_id = dailyId,
-            };
+            //var lunch = new Lunch
+            //{
+            //    TotalCalories = 0,
+            //    TotalCarbs = 0,
+            //    TotalFats = 0,
+            //    TotalProteins = 0,
+            //    DailyPlan_id = dailyId,
+            //};
 
-            var dinner = new Dinner
-            {
-                TotalCalories = 0,
-                TotalCarbs = 0,
-                TotalFats = 0,
-                TotalProteins = 0,
-                DailyPlan_id = dailyId,
-            };
+            //var dinner = new Dinner
+            //{
+            //    TotalCalories = 0,
+            //    TotalCarbs = 0,
+            //    TotalFats = 0,
+            //    TotalProteins = 0,
+            //    DailyPlan_id = dailyId,
+            //};
 
-            // Thêm vào context
-            var breakfastTask = _repositoryManager.User.AddNewBreakfastAsync(breakfast);
-            var lunchTask = _repositoryManager.User.AddNewLunchAsync(lunch);
-            var dinnerTask = _repositoryManager.User.AddNewDinnerAsync(dinner);
-            await Task.WhenAll(breakfastTask, lunchTask, dinnerTask);
+            //// Thêm vào context
+            //var breakfastTask = _repositoryManager.User.AddNewBreakfastAsync(breakfast);
+            //var lunchTask = _repositoryManager.User.AddNewLunchAsync(lunch);
+            //var dinnerTask = _repositoryManager.User.AddNewDinnerAsync(dinner);
+            //await Task.WhenAll(breakfastTask, lunchTask, dinnerTask);
 
-            // DailyPlan
-            var dailyPlan = new DailyPlan
-            {
-                Id = dailyId,
-                User_id = newUser.Id,
-                Breakfast_id = breakfast.Id,
-                Lunch_id = lunch.Id,
-                Dinner_id = dinner.Id,
-                TargetCalories = calo,
-                TargetFats = fat, // gram
-                TargetProteins = protein, // gram
-                TargetCarbs = (calo - protein * 4 - fat * 9) / 4,
-                TotalCalories = 0,
-                TotalFats = 0,
-                TotalProteins = 0,
-                Date = today
-            };
+            //// DailyPlan
+            //var dailyPlan = new DailyPlan
+            //{
+            //    Id = dailyId,
+            //    User_id = newUser.Id,
+            //    Breakfast_id = breakfast.Id,
+            //    Lunch_id = lunch.Id,
+            //    Dinner_id = dinner.Id,
+            //    TargetCalories = calo,
+            //    TargetFats = fat, // gram
+            //    TargetProteins = protein, // gram
+            //    TargetCarbs = (calo - protein * 4 - fat * 9) / 4,
+            //    TotalCalories = 0,
+            //    TotalFats = 0,
+            //    TotalProteins = 0,
+            //    Date = today
+            //};
 
-            await _repositoryManager.User.AddNewDailyPlanAsync(dailyPlan);
+            //await _repositoryManager.User.AddNewDailyPlanAsync(dailyPlan);
 
             await _repositoryManager.SaveAsync();
             return _mapper.Map<NewUserResponseDTO>(newUser);
